dcffd559a16a04decbb535f6172f8b76c95824a7
Eric Defore <eric.defore@gmail.com>
2018-02-15 09:13:11 -0500

    Front page coming together

--- front-page.php
+++ front-page.php
@@ -1,8 +1,9 @@
 <?php
 /**
  * Frontpage Template
+ * @since {{VERSION}}
  *
- * @package GreaterJacksonHabitatTheme2018
+ * @package RBMTheme
  */
 
 global $post;
@@ -12,14 +13,14 @@ get_header();
 while ( have_posts() ) : the_post(); ?>
 
 	<header class="front-hero" role="banner">
-		<div class="marketing">
+		<div class="marketing main-wrap">
 
 			<div class="image">
 				<div class="color-overlay"></div>
 			</div>
 			
 			<div class="expanded row tagline">
-				<div class="small-12 medium-6 columns">
+				<div class="small-12 columns">
 					<?php the_content(); ?>
 				</div>
 			</div>
@@ -27,6 +28,12 @@ while ( have_posts() ) : the_post(); ?>
 		</div>
 
 	</header>
+
+	 <div class="main-wrap row case-studies-header">
+		 <div class="small-12 columns text-center">
+			<?php //echo apply_filters( 'the_content', rbm_theme_get_field( 'case_studies_header' ) ); ?>
+		 </div>
+	 </div>
 
 <?php endwhile;
 
