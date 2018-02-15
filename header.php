f4489c9c2156e02920fb172d8be8c28277bb3d9b
Eric Defore <eric.defore@gmail.com>
2018-02-15 09:00:10 -0500

    Nav bar

--- header.php
+++ header.php
@@ -22,40 +22,46 @@
 
 	<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 
-	<header class="site-header" role="banner">
-		<div class="site-title-bar title-bar">
-			<div class="title-bar-left">
-				<button class="menu-icon<?php echo ( is_front_page() ? ' dark' : '' ); ?>" type="button" data-toggle="off-canvas-menu"></button>
-				<span class="site-mobile-title title-bar-title show-for-small-only">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						
-						<?php if ( is_front_page() ) : ?>
-							
-						<?php else : ?>
-							
-						<?php endif; ?>
-						
-					</a>
-				</span>
+	<header class="site-header" role="banner" data-sticky-container>
+		<div class="sticky-top-bar" data-sticky data-options="anchor: page; marginTop: 0; stickyOn: small;">
+			
+			<div class="row">
+				<div class="small-12 columns">
+				
+					<div class="site-title-bar title-bar">
+						<div class="title-bar-left">
+							<span class="site-mobile-title title-bar-title show-for-small-only">
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+
+									<div class="svg-container">
+										<?php echo file_get_contents( THEME_DIR . '/dist/assets/images/GreaterJackson_Hz_Black.svg' ); ?>
+									</div>
+
+								</a>
+							</span>
+                            <button class="alignright menu-icon<?php echo ( is_front_page() ? ' dark' : '' ); ?>" type="button" data-toggle="off-canvas-menu"></button>
+						</div>
+					</div>
+
+					<nav class="site-navigation top-bar" role="navigation">
+						<div class="site-desktop-title top-bar-title hide-for-small-only">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+
+								<div class="svg-container">
+									<?php echo file_get_contents( THEME_DIR . '/dist/assets/images/GreaterJackson_Hz_Black.svg' ); ?>
+								</div>
+
+							</a>
+						</div>
+						<div class="top-bar-right">
+							<?php foundationpress_top_bar_r(); ?>
+						</div>
+					</nav>
+					
+				</div>
 			</div>
+			
 		</div>
-
-		<nav class="site-navigation top-bar" role="navigation">
-			<div class="site-desktop-title top-bar-title hide-for-small-only">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-
-					<?php if ( is_front_page() ) : ?>
-						
-					<?php else : ?>
-						
-					<?php endif; ?>
-
-				</a>
-			</div>
-			<div class="top-bar-left">
-				<?php foundationpress_top_bar_r(); ?>
-			</div>
-		</nav>
 		
 	</header>
 
