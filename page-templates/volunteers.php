55525cc6d94dbaba538c91cfa579577b95c83eb4
Eric Defore <eric.defore@gmail.com>
2018-02-15 15:26:55 -0500

    Volunteers page basically set

--- page-templates/volunteers.php
+++ page-templates/volunteers.php
@@ -11,10 +11,16 @@ do_action( 'foundationpress_before_content' ); ?>
 <section class="intro" role="main">
 	<div class="fp-intro">
 
-		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-			<div class="entry-content">
-				<?php the_content(); ?>
+		<div <?php post_class( array( 'expanded', 'row' ) ) ?> id="post-<?php the_ID(); ?>" data-equalizer data-equalize-on="small" data-equalize-on-stack="true">
+			<div class="small-12 medium-7 columns content" data-equalizer-watch>
+				<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+			</div>
+			<div class="small-12 medium-5 columns image-container" data-equalizer-watch>
+				<div class="image" style="background-image: url('<?php echo wp_get_attachment_image_url( get_post_thumbnail_id( get_the_ID() ), 'full' ); ?>')">
+				</div>
 			</div>
 		</div>
 
