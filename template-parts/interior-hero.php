47246bd46846b305070a824da5feda61900867be
Eric Defore <eric.defore@gmail.com>
2018-03-02 10:16:17 -0500

    Same fixes for Single Event

--- template-parts/interior-hero.php
+++ template-parts/interior-hero.php
@@ -20,17 +20,23 @@
 					
 					<?php
 					
-					// There's no reason why get_post_type() shouldn't work, but Events Calendar is weird in that the Archive is technically a Page but still reports as an Archive and uses Archive CSS Classes
+					// There's no reason why get_post_type() shouldn't work, but Events Calendar is weird in that the Archive is technically rendered as a Page but still reports as an Archive and uses Archive CSS Classes
 					// Even though the Post Type is defined in $wp_query, get_post_type() thinks it is a Page
 					global $wp_query;
+					global $post;
 
 					if ( is_archive() && 
 						$wp_query->query['post_type'] == 'tribe_events' ) {
 
-						$post_type = get_post_type_object( 'tribe_events' );
+						echo tribe_get_event_label_plural();
 
-						echo $post_type->labels->name;
-
+					}
+					elseif ( is_single() && 
+						   $wp_query->query['post_type'] == 'tribe_events' ) {
+						
+						// Same problem with get_the_ID()/the_title() really
+						echo get_the_title( $wp_query->queried_object_id );
+						
 					}
 					else {
 						the_title();
@@ -39,7 +45,9 @@
 					?>
 					
 				</h1>
+				
 				<?php echo apply_filters( 'the_content', get_bloginfo( 'description' ) ); ?>
+				
 			</div>
 		</div>
 
