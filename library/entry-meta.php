1185e768e91d95e7400b0dfb8fac4a251d917d19
Eric Defore <eric.defore@gmail.com>
2018-06-14 10:57:04 -0400

    Closes #12

--- library/entry-meta.php
+++ library/entry-meta.php
@@ -7,9 +7,16 @@
  */
 
 if ( ! function_exists( 'foundationpress_entry_meta' ) ) :
+
 	function foundationpress_entry_meta() {
+		
+		if ( get_post_type() == 'programs' || 
+		   get_post_type() == 'projects' ) return;
+		
 		/* translators: %1$s: current date, %2$s: current time */
 		echo '<time class="updated" datetime="' . get_the_time( 'c' ) . '">' . sprintf( __( 'Posted on %1$s at %2$s.', 'greater-jackson-habitat-theme' ), get_the_date(), get_the_time() ) . '</time>';
 		echo '<p class="byline author">' . __( 'Written by', 'greater-jackson-habitat-theme' ) . ' <a href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" rel="author" class="fn">' . get_the_author() . '</a></p>';
+		
 	}
+
 endif;
