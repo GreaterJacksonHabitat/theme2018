e9750f2163b2750aa5eab10b642bb0295edc082d
Eric Defore <eric.defore@gmail.com>
2019-06-20 14:37:34 -0400

    Home Page Section building. #31

--- front-page.php
+++ front-page.php
@@ -12,25 +12,47 @@ get_header();
 
 while ( have_posts() ) : the_post(); ?>
 
-	<header class="featured-hero front-hero" role="banner">
+	<header class="featured-hero front-hero" role="banner" style="background-image: url('<?php echo the_post_thumbnail_url( 'full' ); ?>');">
 		<div class="marketing main-wrap">
-			
-			<?php if ( has_post_thumbnail() ) : ?>
-			
-				<div class="image" style="background-image: url('<?php echo the_post_thumbnail_url('full'); ?>;">
-				</div>
-			
-			<?php endif; ?>
-			
 			<div class="row tagline">
 				<div class="small-12 columns">
 					<?php the_content(); ?>
 				</div>
 			</div>
-
 		</div>
-
 	</header>
+
+	<?php 
+
+	$even = false;
+	$count = 1; // Not zero indexed to make modulus make more sense
+	
+	if ( have_rows( 'sections' ) ) : 
+
+		while ( have_rows( 'sections' ) ) : the_row();
+
+			if ( $count % 2 == 0 ) {
+				$even = true;
+			}
+			else {
+				$even = false;
+			}
+
+			$section_type = get_row_layout();
+
+			switch ( $section_type ) {
+
+				case 'leftright_block':
+					include locate_template( 'template-parts/home-sections/leftright-block.php', false, false );
+					break;
+
+			}
+
+			$count++;
+
+		endwhile;
+
+	endif; ?>
 
 <?php endwhile;
 
