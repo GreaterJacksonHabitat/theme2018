9b2a1b7b8c3ffaa78cfcaa859ad8d2f205ce17b4
Eric Defore <eric.defore@gmail.com>
2020-02-11 12:50:41 -0500

    Fix Image Block for new usage

--- template-parts/home-sections/image_block.php
+++ template-parts/home-sections/image_block.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <div class="img-container">
-	<a href="<?php echo esc_attr( get_sub_field( 'image_link' ) ); ?>" class="image_link"?>
+	<a href="<?php echo esc_attr( get_sub_field( 'button_url' ) ); ?>" class="image_link"<?php echo ( ! empty( get_sub_field( 'open_button_in_new_tab' ) ) ? ' target="_blank"' : '' ); ?>>
 		<div class="background-image" style="background-image: url('<?php echo esc_attr( wp_get_attachment_image_url( get_sub_field( 'background_image'), 'full' ) ); ?>');">
 		</div>
 	</a>
