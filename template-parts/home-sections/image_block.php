b1d6ba948de8d098443022ee9c6736a23a872d08
Eric Defore <eric.defore@gmail.com>
2020-03-25 14:54:33 -0400

    Adjust Image Block to allow overlaid text

--- template-parts/home-sections/image_block.php
+++ template-parts/home-sections/image_block.php
@@ -11,9 +11,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-<div class="img-container">
-	<a href="<?php echo esc_attr( get_sub_field( 'button_url' ) ); ?>" class="image_link"<?php echo ( ! empty( get_sub_field( 'open_button_in_new_tab' ) ) ? ' target="_blank"' : '' ); ?>>
+<div class="image-container">
+
+	<?php 
+
+	$link = get_sub_field( 'button_url' );
+
+	if ( $link ) : ?>
+
+		<a href="<?php echo esc_attr( get_sub_field( 'button_url' ) ); ?>" class="image-link"<?php echo ( ! empty( get_sub_field( 'open_button_in_new_tab' ) ) ? ' target="_blank"' : '' ); ?>>
+
+	<?php endif; ?>
+
 		<div class="background-image" style="background-image: url('<?php echo esc_attr( wp_get_attachment_image_url( get_sub_field( 'background_image'), 'full' ) ); ?>');">
 		</div>
-	</a>
+
+	<?php if ( $link ) : ?>
+		</a>
+	<?php endif; ?>
+
+	<?php if ( $text = get_sub_field( 'image_text' ) ) : ?>
+
+		<div class="image-text">
+			<?php echo apply_filters( 'the_content', $text ); ?>
+		</div>
+
+	<?php endif; ?>
+
 </div>
